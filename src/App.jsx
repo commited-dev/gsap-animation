ee60a97cc2710b870c5597f82436a37aa455e741
commited_dev <devcommited@gmail.com>
2025-11-06 10:11:41 +0100

    create a utility class for our headline

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div className="flex-center">
+    <div className="flex-center h-screen">
       <h1 className="text-5xl text-blue-500">GSAP Animation</h1>
     </div>
   );
