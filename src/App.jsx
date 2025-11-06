3efdc2bc85dad75550de93da596a7132afa70458
commited_dev <devcommited@gmail.com>
2025-11-06 10:02:31 +0100

    add fonts and variables for styling

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,11 @@ import { SplitText } from "gsap/SplitText";
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
-  return <div className="text-3xl text-blue-500">GSAP Animation</div>;
+  return (
+    <div className="flex-center">
+      <h1 className="text-5xl text-blue-500">GSAP Animation</h1>
+    </div>
+  );
 };
 
 export default App;
