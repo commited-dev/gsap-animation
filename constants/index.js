31418c3aad4374d1a7f957e84f0faac0344e456a
commited_dev <devcommited@gmail.com>
2025-11-10 09:26:06 +0100

    implement the menu section and the animate the cocktails

--- constants/index.js
+++ constants/index.js
@@ -134,7 +134,7 @@ const socials = [
   },
 ];
 
-const sliderLists = [
+const cocktailMenuList = [
   {
     id: 1,
     name: "Classic Mojito",
@@ -179,5 +179,5 @@ export {
   openingHours,
   storeInfo,
   socials,
-  sliderLists,
+  cocktailMenuList,
 };
